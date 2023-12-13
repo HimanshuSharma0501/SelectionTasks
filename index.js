1451860a748dbb8d6e63ee78f2eb67f3a1091356
HimanshuSharma0501 <112805022+HimanshuSharma0501@users.noreply.github.com>
2023-12-13 18:00:57 +0530

    Updated index.js
    This was added for round 2 task

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 // Sample data
 const users = {
   john_doe: { email: "john@example.com", password: "password123" },
   // can add more
 };
-
-app.use(bodyParser.json());
+//Sample posts
+let posts = [
+  {
+    id: 1,
+    title: "Post 1",
+    content: "This is the first post.",
+    likes: 0,
+    comments: [],
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    content: "This is the second post.",
+    likes: 0,
+    comments: [],
+  },
+  // can add more
+];
 
 // 1.1 User Registration API
 app.post("/register", (req, res) => {
@@ -49,6 +67,98 @@ app.post("/forget_password", (req, res) => {
     message:
       "Password reset instructions sent to the email associated with the account",
   });
+});
+
+// 2 CRUD
+
+//2.1 Create
+app.post("/posts", (req, res) => {
+  const { title, content } = req.body;
+  const newPost = {
+    id: posts.length + 1,
+    title,
+    content,
+    likes: 0,
+    comments: [],
+  };
+
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+// 2.2 Read
+//  all posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+//  specific post
+app.get("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  res.json(post);
+});
+
+// 2.3 Update
+
+app.put("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((p) => p.id === postId);
+
+  if (postIndex === -1) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  const updatedPost = { ...posts[postIndex], ...req.body };
+  posts[postIndex] = updatedPost;
+
+  res.json(updatedPost);
+});
+
+// 2.4 Delete
+app.delete("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  posts = posts.filter((p) => p.id !== postId);
+
+  res.json({ message: "Post deleted successfully" });
+});
+
+// 3 Likes and Comments
+
+//3.1 Likes
+
+app.post("/posts/:id/like", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((p) => p.id === postId);
+
+  if (postIndex === -1) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  posts[postIndex].likes += 1;
+
+  res.json({ message: "Post liked successfully" });
+});
+
+//3.2 Comments
+
+app.post("/posts/:id/comment", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((p) => p.id === postId);
+
+  if (postIndex === -1) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  const { comment } = req.body;
+  posts[postIndex].comments.push(comment);
+
+  res.json({ message: "Comment added successfully" });
 });
 
 app.listen(port, () => {
